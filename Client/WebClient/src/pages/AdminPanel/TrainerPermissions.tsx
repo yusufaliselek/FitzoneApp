50828abde1ec1db50079a577cc1fdd4724d8b082
Yusuf Ali Selek <yusufaliselek41@gmail.com>
2023-09-07 09:11:31 +0300

    toast refactor

--- Client/WebClient/src/pages/AdminPanel/TrainerPermissions.tsx
+++ Client/WebClient/src/pages/AdminPanel/TrainerPermissions.tsx
@@ -71,6 +71,24 @@ const TrainerPermissions = () => {
         getAllTrainerPermissions();
     }, []);
 
+    const ToastInfo = (title: string) =>
+        Toast.fire({
+            icon: 'info',
+            title: title
+        });
+
+    const ToastError = (title: string) =>
+        Toast.fire({
+            icon: 'error',
+            title: title
+        });
+    
+    const ToastSuccess = (title: string) =>
+        Toast.fire({
+            icon: 'success',
+            title: title
+        });
+
     const goLogin = () => navigate('/login');
 
     const clearToken = () => {
@@ -145,24 +163,15 @@ const TrainerPermissions = () => {
     }
 
     const createTrainerPermission = () => {
-        Toast.fire({
-            icon: 'info',
-            title: 'Yetki ekleniyor...'
-        });
+        ToastInfo('Yetki ekleniyor...');
         FitzoneApi.CreateTrainerPermission(trainerPermissionForm).then(res => {
             console.log(res);
-            Toast.fire({
-                icon: 'success',
-                title: 'Yetki eklendi'
-            });
+            ToastSuccess('Yetki eklendi');
             getAllTrainerPermissions();
             resetTrainerPermissionForm();
         }).catch(err => {
             console.log(err);
-            Toast.fire({
-                icon: 'error',
-                title: 'Yetki eklenemedi'
-            });
+            ToastError('Yetki eklenemedi');
         });
     }
 
@@ -180,24 +189,15 @@ const TrainerPermissions = () => {
     };
 
     const editTrainerPermission = () => {
-        Toast.fire({
-            icon: 'info',
-            title: 'Yetki düzenleniyor...'
-        });
+        ToastInfo('Yetki düzenleniyor...');
         FitzoneApi.UpdateTrainerPermission(trainerPermissionForm).then(res => {
             console.log(res);
-            Toast.fire({
-                icon: 'success',
-                title: 'Yetki düzenlendi'
-            });
+            ToastSuccess('Yetki düzenlendi');
             getAllTrainerPermissions();
             resetTrainerPermissionForm();
         }).catch(err => {
             console.log(err);
-            Toast.fire({
-                icon: 'error',
-                title: 'Yetki düzenlenemedi'
-            });
+            ToastError('Yetki düzenlenemedi');
         });
     }
 
@@ -221,24 +221,15 @@ const TrainerPermissions = () => {
             });
             return;
         }
-        Toast.fire({
-            icon: 'info',
-            title: 'Yetki siliniyor...'
-        });
+        ToastInfo('Yetki siliniyor...');
         resetTrainerPermissionForm();
         FitzoneApi.DeleteTrainerPermission(id).then(res => {
             console.log(res);
-            Toast.fire({
-                icon: 'success',
-                title: 'Yetki silindi'
-            });
+            ToastSuccess('Yetki silindi');
             getAllTrainerPermissions();
         }).catch(err => {
             console.log(err);
-            Toast.fire({
-                icon: 'error',
-                title: 'Yetki silinemedi'
-            });
+            ToastError('Yetki silinemedi');
         });
     }
 
@@ -252,126 +243,126 @@ const TrainerPermissions = () => {
 
     return (
         <div className='flex'>
-        <motion.div className={`${isOpen ? "w-[80%]" : "w-full"} h-[calc(100vh-112px)] overflow-y-auto flex flex-col gap-2 items-center`} layout>
-            <div className='w-full mt-5 flex'>
-                <div className='w-[80%] px-1'>
-                    <TextInput placeholder='Yetki Arayın' value={filterText} onChange={(e) => setFilterText(e.target.value)} />
+            <motion.div className={`${isOpen ? "w-[80%]" : "w-full"} h-[calc(100vh-112px)] overflow-y-auto flex flex-col gap-2 items-center`} layout>
+                <div className='w-full mt-5 flex'>
+                    <div className='w-[80%] px-1'>
+                        <TextInput placeholder='Yetki Arayın' value={filterText} onChange={(e) => setFilterText(e.target.value)} />
+                    </div>
+                    <div className='w-[20%] px-1'>
+                        <FButton text='Yetki Ekle' onClick={handleCreateTrainerPermission} />
+                    </div>
                 </div>
-                <div className='w-[20%] px-1'>
-                    <FButton text='Yetki Ekle' onClick={handleCreateTrainerPermission} />
+                <div className='w-full h-full'>
+                    <DataGrid rows={filteredTrainerPermissions} columns={columns} pageSize={30} localeText={trTR.components.MuiDataGrid.defaultProps.localeText} />
                 </div>
-            </div>
-            <div className='w-full h-full'>
-                <DataGrid rows={filteredTrainerPermissions} columns={columns} pageSize={30} localeText={trTR.components.MuiDataGrid.defaultProps.localeText} />
-            </div>
-        </motion.div>
-        <AnimatePresence>
-            {isOpen && (
-                <motion.div
-                    initial={{ width: 0, opacity: 0, x: '100%' }}
-                    animate={{ width: '20%', opacity: 1, x: '0%' }}
-                    exit={{ width: 0, opacity: 0, x: '-100%' }}
-                    transition={{ duration: 0.3, origin: 1 }}
-                    className='w-[20%] pl-3 pt-5'
-                >
-                    <div className='w-full h-full border border-gray-200 rounded-md flex flex-col px-3 py-2 gap-[3%]'>
-                        <div className='flex justify-between items-center'>
-                            <h1 className='text-center'>{formType === "create" ? "Yeni Yetki Ekleyin" : "Yetkiyi Düzenleyin"}</h1>
-                            <div className='p-1 hover:bg-gray-300 rounded-full cursor-pointer transition-all' onClick={resetTrainerPermissionForm}>
-                                <AiOutlineCloseCircle size={25} className='text-gray-700' />
-                            </div>
-                        </div>
-                        <form action="" className='max-h-[calc(100vh-250px)] overflow-y-auto flex flex-col gap-2'>
-                            <div className='flex flex-col gap-2'>
-                                <div className='px-2 py-1'>
-                                    <TextInput placeholder='Yetki Adı' value={trainerPermissionForm.name} onChange={(e) => setTrainerPermissionForm({ ...trainerPermissionForm, name: e.target.value })} />
+            </motion.div>
+            <AnimatePresence>
+                {isOpen && (
+                    <motion.div
+                        initial={{ width: 0, opacity: 0, x: '100%' }}
+                        animate={{ width: '20%', opacity: 1, x: '0%' }}
+                        exit={{ width: 0, opacity: 0, x: '-100%' }}
+                        transition={{ duration: 0.3, origin: 1 }}
+                        className='w-[20%] pl-3 pt-5'
+                    >
+                        <div className='w-full h-full border border-gray-200 rounded-md flex flex-col px-3 py-2 gap-[3%]'>
+                            <div className='flex justify-between items-center'>
+                                <h1 className='text-center'>{formType === "create" ? "Yeni Yetki Ekleyin" : "Yetkiyi Düzenleyin"}</h1>
+                                <div className='p-1 hover:bg-gray-300 rounded-full cursor-pointer transition-all' onClick={resetTrainerPermissionForm}>
+                                    <AiOutlineCloseCircle size={25} className='text-gray-700' />
                                 </div>
-                                {trainerPermissionParamCheckboxes.map((item, index) => (
-                                    <FormControlLabel
-                                        key={index}
-                                        control={<Checkbox checked={Boolean(trainerPermissionForm[item as keyof IGetTrainerPermissionById])} onChange={handleTrainerPermissionForm} name={item} />}
-                                        label={trainerPermissionParamLabels[index]}
-                                    />
-                                ))}
                             </div>
-                        </form>
-                        <FButton text='Kaydet' theme={formType === "create" ? 'primary' : "success"} onClick={() => saveTrainerPermission(formType)} />
-                    </div>
-                </motion.div>
-            )}
-        </AnimatePresence>
-        <div>
-            <Dialog
-                onClose={setDialogIsOpen}
-                open={dialogIsOpen}
-            >
-                <DialogTitle sx={{ m: 0, p: 2 }}>
-                    Antrenör Yetkisini Sil
-                </DialogTitle>
-                <IconButton
-                    aria-label="close"
-                    sx={{
-                        position: 'absolute',
-                        right: 8,
-                        top: 8,
-                        color: (theme) => theme.palette.grey[500],
-                    }}
-                    onClick={() => setDialogIsOpen(false)}
+                            <form action="" className='max-h-[calc(100vh-250px)] overflow-y-auto flex flex-col gap-2'>
+                                <div className='flex flex-col gap-2'>
+                                    <div className='px-2 py-1'>
+                                        <TextInput placeholder='Yetki Adı' value={trainerPermissionForm.name} onChange={(e) => setTrainerPermissionForm({ ...trainerPermissionForm, name: e.target.value })} />
+                                    </div>
+                                    {trainerPermissionParamCheckboxes.map((item, index) => (
+                                        <FormControlLabel
+                                            key={index}
+                                            control={<Checkbox checked={Boolean(trainerPermissionForm[item as keyof IGetTrainerPermissionById])} onChange={handleTrainerPermissionForm} name={item} />}
+                                            label={trainerPermissionParamLabels[index]}
+                                        />
+                                    ))}
+                                </div>
+                            </form>
+                            <FButton text='Kaydet' theme={formType === "create" ? 'primary' : "success"} onClick={() => saveTrainerPermission(formType)} />
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+            <div>
+                <Dialog
+                    onClose={setDialogIsOpen}
+                    open={dialogIsOpen}
                 >
-                    <GridCloseIcon />
-                </IconButton>
-                <DialogContent dividers>
-                    <div className='flex flex-col gap-5'>
-                        <div className='flex gap-2'>
-                            <div>
-                                <span className='font-semibold'>Yetki Adı:</span>
+                    <DialogTitle sx={{ m: 0, p: 2 }}>
+                        Antrenör Yetkisini Sil
+                    </DialogTitle>
+                    <IconButton
+                        aria-label="close"
+                        sx={{
+                            position: 'absolute',
+                            right: 8,
+                            top: 8,
+                            color: (theme) => theme.palette.grey[500],
+                        }}
+                        onClick={() => setDialogIsOpen(false)}
+                    >
+                        <GridCloseIcon />
+                    </IconButton>
+                    <DialogContent dividers>
+                        <div className='flex flex-col gap-5'>
+                            <div className='flex gap-2'>
+                                <div>
+                                    <span className='font-semibold'>Yetki Adı:</span>
+                                </div>
+                                <div>
+                                    <span>{trainerPermissionForm.name}</span>
+                                </div>
+                            </div>
+                            <div className='flex gap-2'>
+                                <div>
+                                    <span className='font-semibold'>Yetkiyi Kullanan Antrenör Sayısı:</span>
+                                </div>
+                                <div>
+                                    <span>{trainers.length}</span>
+                                </div>
                             </div>
                             <div>
-                                <span>{trainerPermissionForm.name}</span>
+                                <span className='font-semibold'>Yetkiyi Kullanan Antrenörler:</span>
+                                <div>
+                                    {trainers.length > 0 ? trainers.map((item, index) => (
+                                        <div key={index}>
+                                            <span>{item.name}</span>
+                                        </div>
+                                    ))
+                                        :
+                                        <span>Yetkiyi kullanan antrenör bulunamadı</span>
+                                    }
+                                </div>
+                            </div>
+                            <div>
+                                <span className='font-semibold'>Yetkiler:</span>
+                                <div>
+                                    {trainerPermissionParamCheckboxes.map((item, index) => (
+                                        <div key={index} className={`flex items-center gap-2 ${index === 0 ? "" : "border-t"} py-1`}>
+                                            <span>{trainerPermissionParamLabels[index]}</span>
+                                            <span>{Boolean(trainerPermissionForm[item as keyof IGetTrainerPermissionById]) ? <BiCheckboxChecked size={20} color='green' /> : <BiCheckbox size={20} color='red' />}</span>
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
                         </div>
-                        <div className='flex gap-2'>
-                            <div>
-                                <span className='font-semibold'>Yetkiyi Kullanan Antrenör Sayısı:</span>
-                            </div>
-                            <div>
-                                <span>{trainers.length}</span>
-                            </div>
-                        </div>
-                        <div>
-                            <span className='font-semibold'>Yetkiyi Kullanan Antrenörler:</span>
-                            <div>
-                                {trainers.length > 0 ? trainers.map((item, index) => (
-                                    <div key={index}>
-                                        <span>{item.name}</span>
-                                    </div>
-                                ))
-                                    :
-                                    <span>Yetkiyi kullanan antrenör bulunamadı</span>
-                                }
-                            </div>
-                        </div>
-                        <div>
-                            <span className='font-semibold'>Yetkiler:</span>
-                            <div>
-                                {trainerPermissionParamCheckboxes.map((item, index) => (
-                                    <div key={index} className={`flex items-center gap-2 ${index === 0 ? "" : "border-t"} py-1`}>
-                                        <span>{trainerPermissionParamLabels[index]}</span>
-                                        <span>{Boolean(trainerPermissionForm[item as keyof IGetTrainerPermissionById]) ? <BiCheckboxChecked size={20} color='green' /> : <BiCheckbox size={20} color='red' />}</span>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </DialogContent>
-                <DialogActions>
-                    <Button variant='contained' color='error' onClick={() => deleteTrainerPermission(trainerPermissionForm.id, trainers.length)}>
-                        Yetkiyi Sil
-                    </Button>
-                </DialogActions>
-            </Dialog>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant='contained' color='error' onClick={() => deleteTrainerPermission(trainerPermissionForm.id, trainers.length)}>
+                            Yetkiyi Sil
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
         </div>
-    </div>
     )
 }
 
